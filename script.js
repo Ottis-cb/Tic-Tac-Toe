2942d20fa47ecd815d08e49df4f23c7a179e3770
ottis-cb <fidimanjakasoaottis2008@gmail.com>
2026-08-02 21:43:39 +0300

    feat: Amélioration du JS

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //JOueurs
-let joeur = {
+let joueur = {
     nom: "Ottis",
     symbole: "O",
-    score: 10
+    score: 0
 };
 let cpu = {
     nom: "CPU",
@@ -11,7 +11,7 @@ let cpu = {
 };
 
 let partieTerminee = false;
-let auJOueurDeJouer = true;
+let auJoueurDeJouer = true;
 
 const cases = document.getElementsByClassName("case");
 
@@ -44,10 +44,10 @@ function nombreCasesRemplies(){
 
 
 function afficherScore(){
-    let scoreJoueur = document.querySelector('.score-you');
-    let scoreCpu = document.querySelector('.score-cpu');
+    let scoreJoueur = document.querySelector('.you-score');
+    let scoreCpu = document.querySelector('.cpu-score');
 
-    scoreJoueur.textContent = joeur.score;
+    scoreJoueur.textContent = joueur.score;
     scoreCpu.textContent = cpu.score;
 }
 
@@ -57,9 +57,8 @@ function quigagne(joueurATester) {
         let a=combinaisonGagnates[i][0];
         let b=combinaisonGagnates[i][1];
         let c=combinaisonGagnates[i][2];
-    }
 
-    if(
+        if(
         cases[a].textContent === joueurATester.symbole &&
         cases[b].textContent === joueurATester.symbole &&
         cases[c].textContent === joueurATester.symbole
@@ -68,6 +67,7 @@ function quigagne(joueurATester) {
         cases[b].style.color= "green";
         cases[c].style.color= "green";
         return true;
+        }
     }
     return false;
 }
@@ -83,15 +83,80 @@ function matchNul() {
     afficherScore();
 }
 
-function jouerCase(id){
+function joueurCase(id){
     let caseCliquee = document.getElementById(id);
-
+    let Touraffiche = document.getElementById("TourAffiche");
+    Touraffiche.textContent= "O";
     if(partieTerminee || !auJoueurDeJouer || caseCliquee.classList.contains("coche")){
         return;
     }
+    remplirCase(caseCliquee, joueur.symbole);
+    if(quigagne(joueur)) {
+        finDePartie(joueur);
+        return;
+    }
+    if(nombreCasesRemplies() === 9){
+        matchNul();
+        return;
+    }
+    auJoueurDeJouer = false;
+    joueurCpu();
 }
-remplirCase(caseCliquee, joueur, symbole);
-if(quigagne(joueur)) {
-    finDePartie(joueur);
-    return;
+
+function joueurCpu(){
+    let Touraffiche = document.getElementById("TourAffiche");
+    Touraffiche.textContent= "X";
+    let casesLibres = [];
+    for(let i=0; i<cases.length; i++){
+        if(!cases[i].classList.contains("coche")){
+            casesLibres.push(cases[i]);
+        }
+    }
+
+    let choix = Math.floor(Math.random()*casesLibres.length);
+
+    let caseChoisie = casesLibres[choix];
+
+    remplirCase(caseChoisie, cpu.symbole);
+    if(quigagne(cpu)) {
+        finDePartie(cpu);
+        return;
+    }
+    if(nombreCasesRemplies() === 9){
+        matchNul();
+        return;
+    }
+    auJoueurDeJouer = true;
 }
+for(let i=0; i<cases.length; i++){
+    cases[i].addEventListener("click", function(){
+        joueurCase(cases[i].id);
+    });
+}
+
+const boutonX = document.querySelector('.x-btn');
+const boutonO = document.querySelector('.o-btn');
+boutonX.addEventListener("click", function(){
+    joueur.symbole ="X";
+    cpu.symbole ="O"
+});
+boutonO.addEventListener("click", function(){
+    joueur.symbole ="O";
+    cpu.symbole ="X"
+});
+
+function nouvellePartie1(){
+    for(let i= 0; i<cases.length; i++){
+        cases[i].textContent="";
+        cases[i].classList.remove("coche");
+        cases[i].style.color ="";
+    }
+    partieTerminee = false;
+    auJoueurDeJouer = true;
+}
+
+const nouvellePartieBtn = document.getElementById("NouvellePartie");
+
+nouvellePartieBtn.addEventListener("click", function(){
+    nouvellePartie1();
+});
